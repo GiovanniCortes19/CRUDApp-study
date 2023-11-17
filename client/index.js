989e2ad1bb130360818f5cdcccef256e3187cfd2
GiovanniCortes <giocortes19@gmail.com>
2023-11-17 18:30:32 -0500

    created a signup.html and serving it when user hits the endpoint /signup

--- client/index.js
+++ client/index.js
@@ -1,8 +1,12 @@
+// CREATE MY COMPONENTS?
+
 // WAIT FOR DOM TO LOAD BEFORE RUNNING ANY JS
 document.addEventListener('DOMContentLoaded', () => {
   const title = document.createElement('h1');
   title.innerText = 'Grad Assessment Study: CRUD APP';
+  title.style.textAlign = 'center';
 
   const body = document.querySelector('body');
+  // INSERT TITLE HEADER INSIDE THE BODY
   body.appendChild(title);
 });
--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+// SERVE STATIC FILES
 app.use(express.static(path.join(__dirname, '../client')));
 
+// ROUTES
+// HOME
+app.get('/', (req, res) => {
+  console.log('Got the home page');
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+// SIGNUP
+app.get('/signup', (req, res) => {
+  console.log('Serving the Sign Up page...');
+  res.sendFile(path.join(__dirname, '../client/signup.html'));
+});
+
+// CONNECT TO SERVER
 app.listen(3000, () => {
   console.log('listening on PORT 3000...');
 });
